199c6831fad01e84dd926353c808b3419187d0a7
TSingSea <59871143+TSingSea@users.noreply.github.com>
2024-08-29 09:13:17 +0800

    Update settings.py
    在multi_store/settings.py中，将新创建的stores应用添加到INSTALLED_APPS中;
    将REST API 开发应用添加到INSTALLED_APPS中;

--- multi_store/multi_store/settings.py
+++ multi_store/multi_store/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',  # 如果使用Django REST framework，建议安装和配置
+    'stores',
 ]
 
 MIDDLEWARE = [
